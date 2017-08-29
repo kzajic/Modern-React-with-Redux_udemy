6015800a4e6ba92c4a117421afe85a347907140b
kzajic <k.zajic@outlook.com>
2017-08-29 21:53:17 +0200

    Ajax Request with React

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
-
-    //zamiast props^
-    //const video = props.video;
-
-
+const VideoListItem = ({video, onVideoSelect}) => {
+    const imageUrl = video.snippet.thumbnails.default.url;
     return (
-    <li className='list-group-item'>       
-    </li>
+        <li onClick = {() => onVideoSelect(video)}className ='list-group-item'>  
+            <div className = 'video-list media'>
+                <div className = 'media-left'>
+                    <img className ='media-object' src={imageUrl}/>
+                </div>
+            </div>
+
+            <div className = 'media-body'>
+                <div className = 'media-heading'>{video.snippet.title}</div>
+            </div>    
+        </li>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,25 +1,44 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTsearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_details';
 const API_KEY = "AIzaSyCWAm1hi8hCgG2PaPaCj3apQ539xkxQdC0";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+     };
 
-    YTsearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
-      // this.setState({ videos : videos});
+    this.videoSearch('surboards');
+
+  }
+
+  videoSearch(term) {
+    YTsearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({ 
+        videos : videos,
+        selectedVideo: videos[0]
+       });
+      
     })
+
   }
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
     return (
         <div>
-          <SearchBar />
-          <VideoList videos={this.state.videos}/>
+          <SearchBar onSearchTermChange={videoSearch}/>
+          <VideoDetail video={this.state.selectedVideo}/>
+          <VideoList
+           onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+           videos={this.state.videos}/>
         </div>
       );
     }
